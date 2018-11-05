bc87bea5db3d9f304b411e01ba6a4c908a6ceced
Hasin Hayder <countdraculla@gmail.com>
2018-11-05 20:47:10 +0600

    ui tricks

--- demowidget.php
+++ demowidget.php
@@ -12,6 +12,7 @@ Domain Path: /languages/
 */
 
 require_once plugin_dir_path(__FILE__)."widgets/class.demowidget.php";
+require_once plugin_dir_path(__FILE__)."widgets/class.demowidgetui.php";
 require_once plugin_dir_path(__FILE__)."widgets/class.advertisementwidget.php";
 
 function demowidget_load_textdomain() {
@@ -24,6 +25,7 @@ add_action( 'plugins_loaded', 'demowidget_load_textdomain' );
 function demowidget_register(){
 	register_widget('DemoWidget');
 	register_widget('AdvertisementWidget');
+	register_widget('DemoWidgetUI');
 }
 add_action('widgets_init','demowidget_register');
 
@@ -31,6 +33,7 @@ function demowidget_admin_enqueue_scripts($screen){
 	if($screen=="widgets.php") {
 		wp_enqueue_media();
 		wp_enqueue_script("advertisement-widget-js", plugin_dir_url(__FILE__)."js/media-gallery.js", array("jquery"), "1.0", 1);
+		wp_enqueue_style("demowidget-admin-ui-css",plugin_dir_url(__FILE__)."css/widget.css");
 	}
 }
 
